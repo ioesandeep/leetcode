b53171410b4012297fd84f9bc32e96915911e3e2
Sandeep Giri <ioesandeep@gmail.com>
2019-10-11 22:29:33 -0500

    detect capital

---
+++ src/easy/DetectCapital.java
@@ -0,0 +1,27 @@
+package easy;
+
+public class DetectCapital {
+    public boolean detectCapitalUse(String word) {
+        if (word.length() <= 1) {
+            return true;
+        }
+
+        char first = word.charAt(0);
+        if (first >= 'a' && first <= 'z') {
+            return verifyRange(word, 1, 'a', 'z');
+        }
+        char second = word.charAt(1);
+        return second >= 'a' && second <= 'z' ? verifyRange(word, 2, 'a', 'z') : verifyRange(word, 2, 'A', 'Z');
+    }
+
+    boolean verifyRange(String word, int pos, char start, char end) {
+        if (pos >= word.length()) {
+            return true;
+        }
+        char ch = word.charAt(pos);
+        if (ch < start || ch > end) {
+            return false;
+        }
+        return verifyRange(word, pos + 1, start, end);
+    }
+}
