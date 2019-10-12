fad676b61692621e927fe1f7af7b379b6f76b7a3
Sandeep Giri <ioesandeep@gmail.com>
2019-10-11 22:26:30 -0500

    Two sum IV - input contains BST

---
+++ src/easy/TwoSumIVBST.java
@@ -0,0 +1,25 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoSumIVBST {
+    Set<Integer> set;
+
+    public boolean findTarget(TreeNode root, int k) {
+        set = new HashSet<>();
+        return inorder(root, k);
+    }
+
+    boolean inorder(TreeNode root, int target) {
+        if (root == null) {
+            return false;
+        }
+
+        if (set.contains(target - root.val)) {
+            return true;
+        }
+        set.add(root.val);
+        return inorder(root.left, target) || inorder(root.right, target);
+    }
+}
