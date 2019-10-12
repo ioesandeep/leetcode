b673665e68aff569e65509572401e015c422425c
Sandeep Giri <ioesandeep@gmail.com>
2019-10-12 00:37:31 -0500

    plus one linked list

--- src/easy/ListNode.java
+++ src/easy/ListNode.java
@@ -1,9 +1,9 @@
 package easy;
 
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode prev;
+    public int val;
+    public ListNode next;
+    public ListNode prev;
 
     public ListNode(int val) {
         this.val = val;
