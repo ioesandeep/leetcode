723b9e066f7a0e28365d1d6f6b5348f437dec8ec
Sandeep Giri <ioesandeep@gmail.com>
2019-10-11 22:27:12 -0500

    roman to int

---
+++ src/easy/RomanToInt.java
@@ -0,0 +1,33 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInt {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length()) {
+                if (map.get(s.charAt(i + 1)) > map.get(s.charAt(i))) {
+                    continue;
+                }
+            }
+            if (i > 0) {
+                if (map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
+                    sum -= map.get(s.charAt(i - 1));
+                }
+            }
+            sum += map.get(s.charAt(i));
+        }
+        return sum;
+    }
+}
