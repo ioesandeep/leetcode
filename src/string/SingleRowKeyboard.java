1b73d17b07246bd6e3afa11ff5015f0c4fef5ab8
Sandeep Giri <ioesandeep@gmail.com>
2019-10-11 20:59:43 -0500

    Single row keyboard

---
+++ src/string/SingleRowKeyboard.java
@@ -0,0 +1,26 @@
+package string;
+
+public class SingleRowKeyboard {
+    public int calculateTime(String keyboard, String word) {
+        if (word.length() == 0) {
+            return 0;
+        }
+
+        int[] chars = new int[26];
+        for (int i = 0; i < keyboard.length(); i++) {
+            int ch = keyboard.charAt(i) - 'a';
+            chars[ch] = i;
+        }
+
+        char prev = word.charAt(0);
+        int moves = chars[prev - 'a'];
+
+        for (int i = 1; i < word.length(); i++) {
+            char current = word.charAt(i);
+            moves += Math.abs(chars[prev - 'a'] - chars[current - 'a']);
+            prev = current;
+        }
+
+        return moves;
+    }
+}
