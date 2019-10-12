c92f0d2b53a551fbb14e65f60282c25fe0483a08
Sandeep Giri <ioesandeep@gmail.com>
2019-10-12 04:49:48 -0500

    Binary tree post order traversal

---
+++ src/hard/BinaryTreePostOrder.java
@@ -0,0 +1,30 @@
+package hard;
+
+import easy.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePostOrder {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+
+        postorderHelper(root, list);
+        return list;
+    }
+
+    public void postorderHelper(TreeNode root, List<Integer> list) {
+        if (root.left != null) {
+            postorderHelper(root.left, list);
+        }
+
+        if (root.right != null) {
+            postorderHelper(root.right, list);
+        }
+
+        list.add(root.val);
+    }
+}
