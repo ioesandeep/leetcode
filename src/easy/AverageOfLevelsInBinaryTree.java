843f9bfa0b50dca2568727792caef81df6d81c2b
Sandeep Giri <ioesandeep@gmail.com>
2019-10-11 22:18:03 -0500

    average of levvels in binary tree

---
+++ src/easy/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,42 @@
+package easy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AverageOfLevelsInBinaryTree {
+    List<List<Integer>> data;
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        data = new LinkedList<>();
+        dfs(root, 0);
+        return result();
+    }
+
+    void dfs(TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+
+        if (level == data.size()) {
+            data.add(new LinkedList<Integer>());
+        }
+
+        data.get(level).add(root.val);
+
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
+    }
+
+    List<Double> result() {
+        List<Double> result = new LinkedList<>();
+        for (List<Integer> d : data) {
+            double sum = 0;
+            for (Integer val : d) {
+                sum += val;
+            }
+            result.add(sum / d.size());
+        }
+
+        return result;
+    }
+}
