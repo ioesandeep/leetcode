0b47465b80fb50a49e90477c185bad70b4b84742
Sandeep Giri <ioesandeep@gmail.com>
2019-10-11 22:12:35 -0500

    leaf similar tree

---
+++ src/easy/LeafSimilarTree.java
@@ -0,0 +1,32 @@
+package easy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LeafSimilarTree {
+    List<Integer> result;
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        result = new LinkedList<>();
+        dfs(root1, false);
+        dfs(root2, true);
+        return result.size() == 0;
+    }
+
+    void dfs(TreeNode root1, boolean removeFlag) {
+        if (root1 == null) {
+            return;
+        }
+        dfs(root1.left, removeFlag);
+        if (root1.left == null && root1.right == null) {
+            if (!removeFlag) {
+                result.add(root1.val);
+            } else {
+                if (result.get(0) == root1.val) {
+                    result.remove(0);
+                }
+            }
+        }
+        dfs(root1.right, removeFlag);
+    }
+}
