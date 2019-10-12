957289b6ea218dac5e56259d8e4d5f3743ba7dab
Sandeep Giri <ioesandeep@gmail.com>
2019-10-12 01:10:35 -0500

    minimum number of arrows to burst balloons

---
+++ src/medium/BurstBalloons.java
@@ -0,0 +1,28 @@
+package medium;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class BurstBalloons {
+    public int findMinArrowShots(int[][] points) {
+        Queue<int[]> queue = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        Queue<Integer> balloons = new PriorityQueue<>();
+        for (int i = 0; i < points.length; i++) {
+            queue.add(points[i]);
+        }
+        int arrows = 0;
+        while (!queue.isEmpty()) {
+            int[] point = queue.poll();
+            if (balloons.isEmpty()) {
+                arrows++;
+                balloons.add(point[1]);
+            }
+            if (point[0] > balloons.peek()) {
+                balloons.poll();
+                balloons.add(point[1]);
+                ++arrows;
+            }
+        }
+        return arrows;
+    }
+}
