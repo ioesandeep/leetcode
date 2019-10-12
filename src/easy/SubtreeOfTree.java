87ec1c6c95cda337977e78e56d874d7b13db4f02
Sandeep Giri <ioesandeep@gmail.com>
2019-10-11 23:13:31 -0500

    substree of another tree

---
+++ src/easy/SubtreeOfTree.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class SubtreeOfTree {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        //pre-order traverse the tree
+        if (s == null && t != null) {
+            return false;
+        }
+
+        if (isIdentical(s, t) || isIdentical(s.left, t) || isIdentical(s.right, t)) {
+            return true;
+        }
+
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    public boolean isIdentical(TreeNode root, TreeNode target) {
+        if (root == null && target == null) {
+            return true;
+        }
+
+        if (root == null || target == null) {
+            return false;
+        }
+
+        return root.val == target.val && isIdentical(root.left, target.left) && isIdentical(root.right, target.right);
+    }
+}
