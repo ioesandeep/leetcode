8c57a2687802a49bd18dff0a29d1ec2bdd8e04aa
Sandeep Giri <ioesandeep@gmail.com>
2019-10-11 21:14:34 -0500

    unique number of occurrences

---
+++ src/array/UniqueNumberOfOccurrences.java
@@ -0,0 +1,22 @@
+package array;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class UniqueNumberOfOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for (int a : arr) {
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (!set.add(entry.getValue())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
