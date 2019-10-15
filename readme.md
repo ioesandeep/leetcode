0e4672503dac7e8beeede75b1e293af77092d61f
Sandeep Giri <ioesandeep@gmail.com>
2019-10-15 18:35:00 -0500

    add a readme file

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Leetcode problems
+
+The problems are distributed into three packages based on their difficulty level, namely; easy, medium and hard. I have tried to keep the file names as close to the title as possible. You can refer to the commit message for exact title of the problem. 
--- src/hard/MedianOfTwoSortedArrays.java
+++ src/hard/MedianOfTwoSortedArrays.java
@@ -5,33 +5,33 @@ public class MedianOfTwoSortedArrays {
         int i = 0;
         int j = 0;
         int k = 0;
-        int[] res = new int[a.length + b.length];
+        int[] res = new int[a.length+b.length];
         //merge the arrays and then find the median
-        while (i < a.length || j < b.length) {
-            if (i >= a.length && j < b.length) {
-                while (j < b.length) {
+        while(i < a.length || j < b.length) {
+            if(i >= a.length && j < b.length){
+                while(j < b.length){
                     res[k++] = b[j++];
                 }
                 continue;
             }
-            if (j >= b.length && i < a.length) {
-                while (i < a.length) {
+            if(j >= b.length && i < a.length){
+                while(i < a.length){
                     res[k++] = a[i++];
                 }
                 continue;
             }
-            while (j < b.length && i < a.length && a[i] <= b[j]) {
+            while(j < b.length && i < a.length && a[i] <= b[j]){
                 res[k++] = a[i++];
             }
-            while (j < b.length && i < a.length && a[i] > b[j]) {
+            while(j < b.length && i < a.length && a[i] > b[j]){
                 res[k++] = b[j++];
             }
         }
 
-        if (res.length % 2 == 1) {
-            return res[res.length / 2];
+        if(res.length % 2 == 1) {
+            return res[res.length/2];
         }
         int mid = res.length / 2;
-        return (double) (res[mid - 1] + res[mid]) / 2;
+        return (double) (res[mid-1] + res[mid])/2;
     }
 }
